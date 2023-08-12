87347d0a824ab7f9eb86a95ef8bcfe957b104302
Anna Vetyugova <anna.vetyugova@sabris.ru>
2023-08-12 13:37:36 +0300

    refactor: update_project_code

--- webpack.config.js
+++ webpack.config.js
@@ -27,10 +27,7 @@ module.exports = {
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: './src/images/[name].[hash][ext]',
-        }
+        type: 'asset/resource'
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
