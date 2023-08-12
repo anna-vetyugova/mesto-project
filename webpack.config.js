bd67a04057f98a72af4a61b5e16db5520e0959ee
Anna Vetyugova <anna.vetyugova@sabris.ru>
2023-08-12 12:32:35 +0300

    fix_5

--- webpack.config.js
+++ webpack.config.js
@@ -29,14 +29,14 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/,
         type: 'asset/resource',
         generator: {
-            filename: 'images/[name].[hash][ext]',
+            filename: './src/images/[name].[hash][ext]',
         }
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'fonts/[name].[hash][ext]',
+          filename: './src/vendor/fonts/[name].[hash][ext]',
         }
       },
       {
